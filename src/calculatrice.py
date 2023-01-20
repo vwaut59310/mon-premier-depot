8c5178b7f847230eaf3b16eb9c4ebd951120f081
Vincent Wauters <vwaut59310@gmail.com>
2023-01-20 06:30:07 +0100

    Ajout calculatrice.py

---
+++ src/calculatrice.py
@@ -0,0 +1,19 @@
+def multiplication(x: float, y: float) -> float: 
+    return x * y 
+
+  
+
+def division(x: float, y: float) -> float: 
+    return x / y 
+
+  
+
+def addition(x: float, y: float) -> float: 
+    return x + y 
+
+  
+
+def subtraction(x: float, y: float) -> float: 
+    return x - y 
+
+ 
